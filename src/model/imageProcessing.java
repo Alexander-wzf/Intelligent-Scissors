26ab64ac549c609c4cdd4e09f3da1ad4e0dcbea8
Alex Wang <2214725335@qq.com>
2025-04-23 11:40:01 +0800

    添加方法

--- src/model/imageProcessing.java
+++ src/model/imageProcessing.java
@@ -4,6 +4,14 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
 public class imageProcessing {
+    private static final int[][] sobelX = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    private static final int[][] sobelY = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    /**
+     * 将图像转为int值的灰度矩阵
+     * @param image
+     * @return 灰度矩阵
+     */
     private static int[][] image2gray(BufferedImage image){
         int width = image.getWidth();
         int height = image.getHeight();
@@ -34,5 +42,101 @@ public class imageProcessing {
             }
         }
         return grayMatrix;
+    }
+
+    /**
+     * todo 用sobel算子算x方向梯度
+     * @param grayMatrix
+     * @return 梯度矩阵
+     */
+    private static double[][] gradient(int[][] grayMatrix, int[][] kernel){
+        int rows = grayMatrix.length;
+        int cols = grayMatrix[0].length;
+        double[][] D = new double[rows][cols];
+        int[][] appendZero = new int[rows+2][cols+2];
+
+        // 矩阵补零
+        for (int i = 1; i < rows + 1; i++) {
+            System.arraycopy(grayMatrix[i - 1], 0, appendZero[i], 1, cols + 1 - 1);
+        }
+
+        // 算卷积
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                double d = 0;
+
+                for (int k = 0; k < 2; k++) {
+                    for (int l = 0; l < 2; l++) {
+                        d = d + kernel[k][l]*appendZero[i+k-1][j+l-1];
+                    }
+                }
+
+                D[i][j] = d;
+            }
+        }
+
+        return D;
+    }
+
+    private static double[][] getIx(int[][] grayMatrix){
+        return gradient(grayMatrix,sobelX);
+    }
+    private static double[][] getIy(int[][] grayMatrix){
+        return gradient(grayMatrix,sobelY);
+    }
+
+    /**
+     * 计算梯度大小
+     * @param Ix
+     * @param Iy
+     * @return double[][] 梯度大小矩阵
+     */
+    private static double[][] gradientMagnitude(double[][] Ix, double[][] Iy){
+        int rows = Ix.length;
+        int cols = Ix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Ix[i][j] = Math.sqrt(Math.pow(Ix[i][j], 2) + Math.pow(Iy[i][j], 2));
+            }
+        }
+
+        return Ix;
+    }
+
+    /**
+     * 计算节点权重
+     * @param magnitude
+     * @return 权重矩阵
+     */
+    private static double[][] nodeWeights(double[][] magnitude){
+        double gMax = Max(magnitude);
+        int rows = magnitude.length;
+        int cols = magnitude[0].length;
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                magnitude[i][j] = (gMax - magnitude[i][j]) / gMax;
+            }
+        }
+
+        return magnitude;
+    }
+
+    private static double Max(double[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double max = matrix[0][0];
+
+        for (double[] doubles : matrix) {
+            for (double value : doubles) {
+                if (value > max) {
+                    max = value;
+                }
+            }
+        }
+
+        return max;
     }
 }
